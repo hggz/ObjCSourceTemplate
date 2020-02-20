61e7a7c2fdc2bc317738f48b45838a10ededa189
Hugo Gonzalez <hugogonzalez@granular.ag>
2020-02-20 12:04:47 -0800

    Initial Commit

---
+++ ObjCSourceTemplate/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjCSourceTemplate
+//
+//  Created by Hugo Gonzalez on 2/20/20.
+//  Copyright © 2020 Hugo Gonzalez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
